shalture/shalture

commit a2126db334aa7097d5ce973b541605a29f87af81

operserv/akill: allow akilling unsafe masks
Requires operserv:akill-anymask and an additional FORCE parameter.

modules/operserv/akill.c
@@ -118,12 +118,27 @@ static void os_cmd_akill_add(sourceinfo_t *si, int parc, char *parv[])
 	user_t *u;
 	char *target = parv[0];
 	char *token = strtok(parv[1], " ");
+	bool force = false;
 	char star[] = "*";
 	const char *kuser, *khost;
 	char *treason, reason[BUFSIZE];
 	long duration;
 	char *s;
 	kline_t *k;
+
+	if (!strcasecmp(token, "FORCE"))
+	{
+		if (has_priv(si, PRIV_AKILL_ANYMASK))
+		{
+			force = true;
+			token = strtok(NULL, " ");
+		}
+		else
+		{
+			command_fail(si, fault_noprivs, STR_NO_PRIVILEGE, PRIV_AKILL_ANYMASK);
+			return;
+		}
+	}
 
 	if (!target || !token)
 	{
@@ -268,9 +283,11 @@ static void os_cmd_akill_add(sourceinfo_t *si, int parc, char *parv[])
 			unsafe = true;
 		else if ((p = strrchr(khost, '/')) != NULL && IsDigit(p[1]) && atoi(p + 1) < 4)
 			unsafe = true;
-		if (unsafe)
+		if (unsafe && !force)
 		{
 			command_fail(si, fault_badparams, _("Invalid user@host: \2%s@%s\2. This mask is unsafe."), kuser, khost);
+			if (has_priv(si, PRIV_AKILL_ANYMASK))
+				command_fail(si, fault_badparams, _("Use AKILL ADD %s@%s FORCE to override this restriction."), kuser, khost);
 			logcommand(si, CMDLOG_ADMIN, "failed AKILL ADD \2%s@%s\2 (unsafe mask)", kuser, khost);
 			return;
 		}

commit 51f9b6450e848023d1996e0762e92e388a598239

operserv/akill: fix crash bug introduced in a2126db3
/os AKILL ADD would segfault as it'd try to compare the non-existent
first argument to "FORCE".

modules/operserv/akill.c
@@ -126,7 +126,7 @@ static void os_cmd_akill_add(sourceinfo_t *si, int parc, char *parv[])
 	char *s;
 	kline_t *k;
 
-	if (!strcasecmp(token, "FORCE"))
+	if (token && !strcasecmp(token, "FORCE"))
 	{
 		if (has_priv(si, PRIV_AKILL_ANYMASK))
 		{