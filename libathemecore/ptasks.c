97b32ba67777b4e23b7627ba0c84239ba3b55cfb
Janik Kleinhoff <janik@kleinhoff.de>
2015-09-03 10:40:33 +0000

    ptasks.c: s/atheme.motd/shalture.motd/ (fixes #64)

--- libathemecore/ptasks.c
+++ libathemecore/ptasks.c
@@ -409,7 +409,7 @@ void handle_motd(user_t *u)
 	if (floodcheck(u, NULL))
 		return;
 
-	f = fopen(SYSCONFDIR "/atheme.motd", "r");
+	f = fopen(SYSCONFDIR "/shalture.motd", "r");
 	if (!f)
 	{
 		numeric_sts(me.me, 422, u, ":The MOTD file is unavailable.");
