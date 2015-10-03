6e128b06e8da05ecebd97643abde3c4d44d47994
Colgate Minuette <Colgate@canternet.org>
2015-10-02 22:09:24 -0700

    nickserv: Verify that the nick being regained is valid.

--- modules/nickserv/enforce.c
+++ modules/nickserv/enforce.c
@@ -331,6 +331,12 @@ static void ns_cmd_regain(sourceinfo_t *si, int parc, char *parv[])
 		logcommand(si, CMDLOG_DO, "failed REGAIN \2%s\2 (password authentication disabled)", target);
 		return;
 	}
+	if (!is_valid_nick(target))
+	{
+		command_fail(si, fault_badparams, "\2%s\2 is not a valid nick.", target);
+		logcommand(si, CMDLOG_DO, "failed REGAIN \2%s\2 (not a valid nickname)", target);
+		return;
+	}
 	if ((si->smu == mn->owner) || verify_password(mn->owner, password))
 	{
 		/* identify them to the target nick's account if they aren't yet */
